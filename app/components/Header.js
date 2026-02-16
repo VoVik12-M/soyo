1b108291935b793a153207590f44fbe2002ab106
VoVik12-M <masluk.v.i@gmail.com>
2026-02-16 20:52:50 +0200

    sitka3

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,12 +1,33 @@
 export default function Header() {
+    const viasan = "(Віасан)";
+
     return (
         <div className="Header">
             <div className="Face">
-                <div className="Adresa"></div>
-                <div className="Nomeru"></div>
+                <div className="Adresa">
+                    <p>м. Тернопіль</p>
+                    <p>вул. Медова 12</p>
+                    <p>{viasan}</p>
+                </div>
+                <div className="Nomeru">
+                <div>
+                        <p>Назар:</p>
+                        <p>+380980466115</p>
+                    </div>
+                    <div>
+                        <p>Катя:</p>
+                        <p>+380982402674</p>
+                    </div>
+                </div>
                 <div className="Media"></div>
                 <div className="Logo"></div>
-                <div className="Main"></div>
+                <div className="Main">
+                    <a>СТИЛІ</a>
+                    <a>ПЕДАГОГИ</a>
+                    <a>РОЗКЛАД</a>
+                    <a>ЗАЛ</a>
+                    <a>ЦІНИ</a>
+                </div>
             </div>
         </div>
     );
