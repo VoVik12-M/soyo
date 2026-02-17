7e74fdcc890e004e6fe6e7df29a02a0eac22951c
VoVik12-M <masluk.v.i@gmail.com>
2026-02-17 14:04:24 +0200

    sitka_finish

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,18 @@
 export default function Header() {
     const viasan = "(Віасан)";
 
+    // Картинки
+    // Жовті
+    const homeY = "/logo_yellow/home_yellow.png";
+    const instagramY = "/logo_yellow/instagram_yellow.png";
+    const telegramY = "/logo_yellow/telegram_yellow.png";
+    const tik_tokY ="/logo_yellow/tik-tok_yellow.png";
+    // Сірі
+    const homeG = "/logo_grey/home_grey.png";
+    const instagramG = "/logo_grey/instagram_grey.png";
+    const telegramG = "/logo_grey/telegram_grey.png";
+    const tik_tokG ="/logo_grey/tik-tok_grey.png";
+
     return (
         <div className="Header">
             <div className="Face">
@@ -21,8 +33,15 @@ export default function Header() {
                         <p>+380982402674</p>
                     </div>
                 </div>
-                <div className="Media"></div>
-                <div className="Logo"></div>
+                <div className="Media">
+                    <img src={homeY}/>
+                    <img src={instagramY}/>
+                    <img src={telegramY}/>
+                    <img src={tik_tokY}/>
+                </div>
+                <div className="Logo">
+                    <img src='/img/Logo.png' alt="logo"/>
+                </div>
                 <div className="Main">
                     <a>СТИЛІ</a>
                     <a>ПЕДАГОГИ</a>
