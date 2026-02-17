63995b173bc97d35ca9ad6a354475b7134217f0c
VoVik12-M <masluk.v.i@gmail.com>
2026-02-17 14:45:10 +0200

    efect_main

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRef, useState } from "react";
+
 export default function Header() {
     const viasan = "(Віасан)";
 
@@ -12,6 +16,105 @@ export default function Header() {
     const instagramG = "/logo_grey/instagram_grey.png";
     const telegramG = "/logo_grey/telegram_grey.png";
     const tik_tokG ="/logo_grey/tik-tok_grey.png";
+
+    // Для кнопки СТИЛІ
+    const ef_top = useRef();
+    const ef_right = useRef();
+    const ef_bottom = useRef();
+    const ef_left = useRef();
+
+    function efectu() {
+        ef_top.current.classList.toggle("Top");
+        ef_right.current.classList.toggle("Right");
+        ef_bottom.current.classList.toggle("Bottom");
+        ef_left.current.classList.toggle("Left");
+    }
+
+    function efectudel() {
+        ef_top.current.classList.remove("Top");
+        ef_right.current.classList.remove("Right");
+        ef_bottom.current.classList.remove("Bottom");
+        ef_left.current.classList.remove("Left");
+    }
+    // Для кнопки ПЕДАГОГИ
+    const ef_top_2 = useRef();
+    const ef_right_2 = useRef();
+    const ef_bottom_2 = useRef();
+    const ef_left_2 = useRef();
+
+    function efectu2() {
+        ef_top_2.current.classList.toggle("Top2");
+        ef_right_2.current.classList.toggle("Right2");
+        ef_bottom_2.current.classList.toggle("Bottom2");
+        ef_left_2.current.classList.toggle("Left2");
+    }
+
+    function efectu2del() {
+        ef_top_2.current.classList.remove("Top2");
+        ef_right_2.current.classList.remove("Right2");
+        ef_bottom_2.current.classList.remove("Bottom2");
+        ef_left_2.current.classList.remove("Left2");
+    }
+
+    // Для кнопки РОЗКЛАД
+    const ef_top_3 = useRef();
+    const ef_right_3 = useRef();
+    const ef_bottom_3 = useRef();
+    const ef_left_3 = useRef();
+
+    function efectu3() {
+        ef_top_3.current.classList.toggle("Top3");
+        ef_right_3.current.classList.toggle("Right3");
+        ef_bottom_3.current.classList.toggle("Bottom3");
+        ef_left_3.current.classList.toggle("Left3");
+    }
+
+    function efectu3del() {
+        ef_top_3.current.classList.remove("Top3");
+        ef_right_3.current.classList.remove("Right3");
+        ef_bottom_3.current.classList.remove("Bottom3");
+        ef_left_3.current.classList.remove("Left3");
+    }
+
+    // Для кнопки ЗАЛ
+    const ef_top_4 = useRef();
+    const ef_right_4 = useRef();
+    const ef_bottom_4 = useRef();
+    const ef_left_4 = useRef();
+
+    function efectu4() {
+        ef_top_4.current.classList.toggle("Top4");
+        ef_right_4.current.classList.toggle("Right4");
+        ef_bottom_4.current.classList.toggle("Bottom4");
+        ef_left_4.current.classList.toggle("Left4");
+    }
+
+    function efectu4del() {
+        ef_top_4.current.classList.remove("Top4");
+        ef_right_4.current.classList.remove("Right4");
+        ef_bottom_4.current.classList.remove("Bottom4");
+        ef_left_4.current.classList.remove("Left4");
+    }
+
+    // Для кнопки ЦІНИ
+    const ef_top_5 = useRef();
+    const ef_right_5 = useRef();
+    const ef_bottom_5 = useRef();
+    const ef_left_5 = useRef();
+
+    function efectu5() {
+        ef_top_5.current.classList.toggle("Top5");
+        ef_right_5.current.classList.toggle("Right5");
+        ef_bottom_5.current.classList.toggle("Bottom5");
+        ef_left_5.current.classList.toggle("Left5");
+    }
+
+    function efectu5del() {
+        ef_top_5.current.classList.remove("Top5");
+        ef_right_5.current.classList.remove("Right5");
+        ef_bottom_5.current.classList.remove("Bottom5");
+        ef_left_5.current.classList.remove("Left5");
+    }
 
     return (
         <div className="Header">
@@ -43,11 +146,41 @@ export default function Header() {
                     <img src='/img/Logo.png' alt="logo"/>
                 </div>
                 <div className="Main">
-                    <a>СТИЛІ</a>
-                    <a>ПЕДАГОГИ</a>
-                    <a>РОЗКЛАД</a>
-                    <a>ЗАЛ</a>
-                    <a>ЦІНИ</a>
+                <a onMouseEnter={efectu} onMouseLeave={efectudel} href="">
+                            <span ref={ef_top} className="efectTop"></span>
+                            <span ref={ef_right} className="efectRight"></span>
+                            <span ref={ef_bottom} className="efectBottom"></span>
+                            <span ref={ef_left} className="efectLeft"></span>
+                            СТИЛІ
+                        </a>
+                        <a onMouseEnter={efectu2} onMouseLeave={efectu2del} href="">
+                            <span ref={ef_top_2} className="efectTop2"></span>
+                            <span ref={ef_right_2} className="efectRight2"></span>
+                            <span ref={ef_bottom_2} className="efectBottom2"></span>
+                            <span ref={ef_left_2} className="efectLeft2"></span>
+                            ПЕДАГОГИ
+                        </a>
+                        <a onMouseEnter={efectu3} onMouseLeave={efectu3del} href="">
+                            <span ref={ef_top_3} className="efectTop3"></span>
+                            <span ref={ef_right_3} className="efectRight3"></span>
+                            <span ref={ef_bottom_3} className="efectBottom3"></span>
+                            <span ref={ef_left_3} className="efectLeft3"></span>
+                            РОЗКЛАД
+                        </a>
+                        <a onMouseEnter={efectu4} onMouseLeave={efectu4del} href="">
+                            <span ref={ef_top_4} className="efectTop4"></span>
+                            <span ref={ef_right_4} className="efectRight4"></span>
+                            <span ref={ef_bottom_4} className="efectBottom4"></span>
+                            <span ref={ef_left_4} className="efectLeft4"></span>
+                            ЗАЛ
+                        </a>
+                        <a onMouseEnter={efectu5} onMouseLeave={efectu5del} href="">
+                            <span ref={ef_top_5} className="efectTop5"></span>
+                            <span ref={ef_right_5} className="efectRight5"></span>
+                            <span ref={ef_bottom_5} className="efectBottom5"></span>
+                            <span ref={ef_left_5} className="efectLeft5"></span>
+                            ЦІНИ
+                        </a>
                 </div>
             </div>
         </div>
