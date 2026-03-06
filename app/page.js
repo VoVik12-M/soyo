eefee9d80cbe496ef76c27b31628c38693cd91fb
VoVik12-M <masluk.v.i@gmail.com>
2026-03-06 21:29:15 +0200

    Stuli_finish

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
     <div className="Osnova">
         <Header />
         {/* <Hom /> */}
-        {/* <Stuli /> */}
-        <Pedagogu />
+        <Stuli />
+        {/* <Pedagogu /> */}
         {/* <Rozklad /> */}
         {/* <Zal /> */}
         {/* <China /> */}
